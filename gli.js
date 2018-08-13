966ffe5205080587c9a38408c2f36fa8b5252101
davidsharp <hello@davidsharp.codes>
2018-08-13 14:11:52 +0100

    add prompt for when username isn't provided as an option

--- gli.js
+++ gli.js
@@ -3,13 +3,19 @@
 const {h, render} = require('ink');
 const argv = require('minimist')(process.argv.slice(2));
 const GlitchProjects = require('import-jsx')('./GlitchProjects.jsx', {pragma: 'h'});
+var prompt = require('prompt');
 
 const opts = argv._;
 const pins = argv.p||argv.pins;
 
 if(opts.length<1){
-  console.log('Whoops! You need to pass a username `$ gli davidsharp` 🤦‍')
-  process.exit()
+  //console.log('Whoops! You need to pass a username `$ gli davidsharp` 🤦‍')
+  //process.exit()
+  prompt.message='🎏  '
+  prompt.delimiter=' ~ '
+  prompt.start();
+  prompt.get(['Glitch Username'], function (err, result) {
+    render(h(GlitchProjects,{opts:[result['Glitch Username']],pins}))
+  });
 }
-
-const unmount = render(h(GlitchProjects,{opts,pins}));
+else render(h(GlitchProjects,{opts,pins}));
